a8236a1e39a8e86981766a8ef334f14e3f2591b3
1jiwoo27 <chloey1227@gmail.com>
2026-01-05 14:49:13 +0900

    feat: 구독한 언론사 배지 분리

---
+++ src/components/subBadge.js
@@ -0,0 +1,14 @@
+import { getSubscribedCount } from '../state/subscription.js';
+
+export function initSubscriptionBadge() {
+    const badge = document.querySelector('.sub-badge');
+    function update() {
+        badge.textContent = getSubscribedCount();
+    }
+
+    update();
+
+    return {
+        update,
+    };
+}
