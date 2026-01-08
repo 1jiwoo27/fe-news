1f9ead113dafae737ac1183cae0f42f2b3dde206
1jiwoo27 <chloey1227@gmail.com>
2026-01-08 17:38:19 +0900

    feat: 폴더 분리

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { initRollingNews } from './components/rollingNews.js';
 import { initSubscriptionBadge } from './components/subBadge.js';
-import { initProviderSection }  from './components/providerSection.js';
+import { initProviderSection }  from './sections/providerSection.js';
 
 initRollingNews();
 initSubscriptionBadge();
